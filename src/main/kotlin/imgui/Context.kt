11caad27036e5c2f70e61f02b15d2d41ad807aef
gBarbieri <elect86@gmail.com>
2018-06-26 11:10:33 +0200

    :construction: up to May 17

--- src/main/kotlin/imgui/Context.kt
+++ src/main/kotlin/imgui/Context.kt
@@ -11,6 +11,7 @@ import java.nio.ByteBuffer
 import java.util.*
 import kotlin.collections.ArrayList
 
+/** Main imgui context */
 class Context(sharedFontAtlas: FontAtlas? = null) {
 
     var initialized = false
