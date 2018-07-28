8e798b15c549384b17f2130433dd764732cd048c
elect86 <elect86@gmail.com>
2018-07-28 09:49:40 +0200

    fixed #50 (number of lines)

--- src/main/kotlin/imgui/imgui/demo/columns_.kt
+++ src/main/kotlin/imgui/imgui/demo/columns_.kt
@@ -152,7 +152,7 @@ object columns_ {
                         val ITEMS_COUNT = 2000
                         val clipper = ListClipper(ITEMS_COUNT)  // Also demonstrate using the clipper for large list
                         while (clipper.step())
-                            for (i in clipper.display)
+                            for (i in clipper.display.start until clipper.display.endInclusive)
                                 for (j in 0..9) {
                                     text("Line $i Column $j...")
                                     nextColumn()
