01b08698c5a5be0aebedb20df975fc944e409d87
elect86 <elect86@gmail.com>
2018-09-09 15:42:51 +0200

    :construction: up to Aug 21 (demo included)

--- src/main/kotlin/imgui/imgui/demo/showExampleApp/Console.kt
+++ src/main/kotlin/imgui/imgui/demo/showExampleApp/Console.kt
@@ -312,7 +312,8 @@ object Console {
 //                    // A better implementation would preserve the data on the current input line along with cursor position.
 //                    if (prev_history_pos != HistoryPos)
 //                        {
-//                            data->CursorPos = data->SelectionStart = data->SelectionEnd = data->BufTextLen = (int)snprintf(data->Buf, (size_t)data->BufSize, "%s", (HistoryPos >= 0) ? History[HistoryPos] : "");
+//                            int sz = (int)snprintf(data->Buf, (size_t)data->BufSize, "%s", (HistoryPos >= 0) ? History[HistoryPos] : "");
+//                            data->CursorPos = data->SelectionStart = data->SelectionEnd = data->BufTextLen = sz;
 //                            data->BufDirty = true;
 //                        }
 //                }
