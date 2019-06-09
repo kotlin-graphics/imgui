e79285c5baa1bcfa2368268f72c0fd84595cab1a
Nicholas Johnson <sylvyrfysh@gmail.com>
2019-06-09 00:28:45 -0500

    Fix #97

--- imgui-core/src/main/kotlin/imgui/imgui/internal.kt
+++ imgui-core/src/main/kotlin/imgui/imgui/internal.kt
@@ -3164,7 +3164,7 @@ interface imgui_internal {
                 }
                 /*  If the underlying buffer resize was denied or not carried to the next frame,
                     apply_new_text_length+1 may be >= buf_size.                 */
-                buf.strncpy(applyNewText, applyNewTextLength min buf.size)
+                buf.strncpy(applyNewText, (applyNewTextLength + 1) min buf.size)
                 valueChanged = true
             }
 
