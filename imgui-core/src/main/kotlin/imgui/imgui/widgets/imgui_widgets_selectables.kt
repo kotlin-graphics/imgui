66c97c3881cb5ceee0b1f0f07c5420d90ebcb2aa
gbarbieri <elect86@gmail.com>
2019-08-09 17:20:37 +0200

    new bug found

--- imgui-core/src/main/kotlin/imgui/imgui/widgets/imgui_widgets_selectables.kt
+++ imgui-core/src/main/kotlin/imgui/imgui/widgets/imgui_widgets_selectables.kt
@@ -13,6 +13,7 @@ import imgui.ImGui.currentWindow
 import imgui.ImGui.itemAdd
 import imgui.ImGui.itemSize
 import imgui.ImGui.markItemEdited
+import imgui.ImGui.popColumnsBackground
 import imgui.ImGui.popStyleColor
 import imgui.ImGui.pushColumnsBackground
 import imgui.ImGui.pushStyleColor
@@ -140,7 +141,7 @@ interface imgui_widgets_selectables {
         }
 
         if (flags has Sf.SpanAllColumns && window.dc.currentColumns != null) {
-            pushColumnsBackground()
+            popColumnsBackground()
             bb.max.x -= contentRegionMax.x - maxX
         }
 
