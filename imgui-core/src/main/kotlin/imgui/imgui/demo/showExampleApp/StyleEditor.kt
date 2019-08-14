bf22ee7dd1a77183646c0bf6f11e780e25f7c30a
gbarbieri <elect86@gmail.com>
2019-08-14 17:40:49 +0200

    :construction:          Style: Added style.ColorButtonButton (left/right, defaults to ImGuiDir_Right) to move the color button of ColorEdit3/ColorEdit4 functions to either side of the inputs.

--- imgui-core/src/main/kotlin/imgui/imgui/demo/showExampleApp/StyleEditor.kt
+++ imgui-core/src/main/kotlin/imgui/imgui/demo/showExampleApp/StyleEditor.kt
@@ -155,9 +155,16 @@ object StyleEditor {
                 sliderFloat("TabRounding", style::tabRounding, 0f, 12f, "%.0f")
                 text("Alignment")
                 sliderVec2("WindowTitleAlign", style.windowTitleAlign, 0f, 1f, "%.2f")
-                imgui_internal._i = style.windowMenuButtonPosition.i
-                combo("WindowMenuButtonPosition", imgui_internal.Companion::_i, "Left\u0000Right\u0000")
-                style.windowMenuButtonPosition = Dir.values().first { it.i == imgui_internal._i }
+                run {
+                    imgui_internal._i = style.windowMenuButtonPosition.i
+                    combo("WindowMenuButtonPosition", imgui_internal.Companion::_i, "Left\u0000Right\u0000")
+                    style.windowMenuButtonPosition = Dir.values().first { it.i == imgui_internal._i }
+                }
+                run {
+                    imgui_internal._i = style.colorButtonPosition.i
+                    combo("ColorButtonPosition", imgui_internal.Companion::_i, "Left\u0000Right\u0000")
+                    style.colorButtonPosition = Dir.values().first { it.i == imgui_internal._i }
+                }
                 sliderVec2("ButtonTextAlign", style.buttonTextAlign, 0f, 1f, "%.2f"); sameLine(); helpMarker("Alignment applies when a button is larger than its text content.")
                 sliderVec2("SelectableTextAlign", style.selectableTextAlign, 0f, 1f, "%.2f"); sameLine(); helpMarker("Alignment applies when a selectable is larger than its text content.")
                 text("Safe Area Padding"); sameLine(); helpMarker("Adjust if you cannot see the edges of your screen (e.g. on a TV where scaling has not been configured).")
