014783fe63c3c89ff25603a2475b83a0df73e777
gbarbieri <elect86@gmail.com>
2019-08-15 13:30:19 +0200

    :construction:       Increased IMGUI_VERSION_NUM to facilitate transition of OSX clipboard support for framework using/embedding any version of imgui. Amend 29d9394. (#2546)

--- imgui-core/src/main/kotlin/imgui/imgui.kt
+++ imgui-core/src/main/kotlin/imgui/imgui.kt
@@ -16,7 +16,7 @@ const val IMGUI_BUILD = 0
 const val IMGUI_VERSION = "1.72 WIP"// build: $IMGUI_BUILD"
 /** Integer encoded as XYYZZ for use in #if preprocessor conditionals.
 Work in progress versions typically starts at XYY99 then bounce up to XYY00, XYY01 etc. when release tagging happens) */
-const val IMGUI_VERSION_NUM = 17101
+const val IMGUI_VERSION_NUM = 17102
 
 
 // Helpers macros to generate 32-bits encoded colors
