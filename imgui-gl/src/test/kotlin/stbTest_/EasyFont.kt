b5b7a40112607a1a61e118384765d62e45e49be0
gbarbieri <elect86@gmail.com>
2020-01-27 12:54:56 +0100

    easyFont fix

--- imgui-gl/src/test/kotlin/stbTest_/EasyFont.kt
+++ imgui-gl/src/test/kotlin/stbTest_/EasyFont.kt
@@ -10,16 +10,18 @@ fun main() {
     EasyFont("doc/README.md").run("STB Easy Font Demo.kt")
 }
 
+val BASE_HEIGHT = 12
+
 /** STB Easy Font demo.  */
 class EasyFont(filePath: String) : FontDemo(BASE_HEIGHT, filePath) {
     override fun loop() {
-        val charBuffer = Buffer(text.length * 270)
+        val charBuffer = Buffer(text.length * easyFont.averageBytesPerChar)
         val quads = easyFont.print(text = getText(), vertexBuffer = charBuffer)
         GL11.glEnableClientState(GL11.GL_VERTEX_ARRAY)
         GL11.glVertexPointer(2, GL11.GL_FLOAT, 16, charBuffer)
         GL11.glClearColor(43f / 255f, 43f / 255f, 43f / 255f, 0f) // BG color
         GL11.glColor3f(169f / 255f, 183f / 255f, 198f / 255f) // Text color
-        while (!GLFW.glfwWindowShouldClose(getWindow())) {
+        while (!GLFW.glfwWindowShouldClose(window)) {
             GLFW.glfwPollEvents()
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT)
             val scaleFactor = 1.0f + getScale() * 0.25f
@@ -27,15 +29,11 @@ class EasyFont(filePath: String) : FontDemo(BASE_HEIGHT, filePath) {
             // Zoom
             GL11.glScalef(scaleFactor, scaleFactor, 1f)
             // Scroll
-            GL11.glTranslatef(4.0f, 4.0f - getLineOffset() * getFontHeight(), 0f)
+            GL11.glTranslatef(4.0f, 4.0f - lineOffset * fontHeight, 0f)
             GL11.glDrawArrays(GL11.GL_QUADS, 0, quads * 4)
             GL11.glPopMatrix()
-            GLFW.glfwSwapBuffers(getWindow())
+            GLFW.glfwSwapBuffers(window)
         }
         GL11.glDisableClientState(GL11.GL_VERTEX_ARRAY)
-    }
-
-    companion object {
-        private const val BASE_HEIGHT = 12
     }
 }
