ea96b8a1dca08808bc5e04d75cff2c0d3d112f96
Giuseppe Barbieri <elect86@gmail.com>
2020-02-04 23:41:51 +0100

    :construction:     Nav: Fixed a bug where the initial CTRL-Tab press while in a child window sometimes selected the current root window instead of always selecting the previous root window. (#787)

--- imgui-core/src/main/kotlin/imgui/static/navigation.kt
+++ imgui-core/src/main/kotlin/imgui/static/navigation.kt
@@ -357,8 +357,8 @@ fun navUpdateWindowing() {
     val startWindowingWithKeyboard = g.navWindowingTarget == null && io.keyCtrl && Key.Tab.isPressed && io.configFlags has ConfigFlag.NavEnableKeyboard
     if (startWindowingWithGamepad || startWindowingWithKeyboard)
         (g.navWindow ?: findWindowNavFocusable(g.windowsFocusOrder.lastIndex, -Int.MAX_VALUE, -1))?.let {
-            g.navWindowingTarget = it
-            g.navWindowingTargetAnim = it
+            g.navWindowingTarget = it.rootWindow // FIXME-DOCK: Will need to use RootWindowDockStop
+            g.navWindowingTargetAnim = it.rootWindow // FIXME-DOCK: Will need to use RootWindowDockStop
             g.navWindowingHighlightAlpha = 0f
             g.navWindowingTimer = 0f
             g.navWindowingToggleLayer = !startWindowingWithKeyboard
