6d27ede04d90f711f4bb6b8fa103f7b046ed72b5
gbarbieri <elect86@gmail.com>
2020-02-27 16:51:20 +0100

    Revert ":construction:    Version 1.76 WIP"
    This reverts commit f76bd99f

--- imgui-core/src/main/kotlin/imgui/imgui.kt
+++ imgui-core/src/main/kotlin/imgui/imgui.kt
@@ -18,7 +18,7 @@ const val IMGUI_BUILD = 0
 const val IMGUI_VERSION = "1.75"// build: $IMGUI_BUILD"
 /** Integer encoded as XYYZZ for use in #if preprocessor conditionals.
 Work in progress versions typically starts at XYY99 then bounce up to XYY00, XYY01 etc. when release tagging happens) */
-const val IMGUI_VERSION_NUM = 17500
+const val IMGUI_VERSION_NUM = 17501
 
 
 // Helpers macros to generate 32-bits encoded colors
