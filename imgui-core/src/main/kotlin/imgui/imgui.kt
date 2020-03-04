bf6ed0fb16bd08e66f59a5c9534aab7bcecabff1
gbarbieri <elect86@gmail.com>
2020-03-04 10:57:19 +0100

    :construction:    Version 1.76 WIP

--- imgui-core/src/main/kotlin/imgui/imgui.kt
+++ imgui-core/src/main/kotlin/imgui/imgui.kt
@@ -15,10 +15,10 @@ import kool.Stack
 const val IMGUI_BUILD = 0
 
 /** get the compiled version string e.g. "1.23" (essentially the compiled value for IMGUI_VERSION) */
-const val IMGUI_VERSION = "1.75"// build: $IMGUI_BUILD"
+const val IMGUI_VERSION = "1.76 WIP"// build: $IMGUI_BUILD"
 /** Integer encoded as XYYZZ for use in #if preprocessor conditionals.
 Work in progress versions typically starts at XYY99 then bounce up to XYY00, XYY01 etc. when release tagging happens) */
-const val IMGUI_VERSION_NUM = 17501
+const val IMGUI_VERSION_NUM = 17502
 
 
 // Helpers macros to generate 32-bits encoded colors
