1c09252ad53d0b11a634a918f83cf29e4219a4eb
Nicholas Johnson <sylvyrfysh@gmail.com>
2020-05-11 12:47:54 -0500

    Use Interface Delegation for ImplBestGL

--- imgui-gl/src/main/kotlin/imgui/impl/gl/ImplBestGL.kt
+++ imgui-gl/src/main/kotlin/imgui/impl/gl/ImplBestGL.kt
@@ -1,36 +1,27 @@
 package imgui.impl.gl
 
-import imgui.internal.DrawData
 import org.lwjgl.opengl.GL
 import org.lwjgl.system.Platform
 
-class ImplBestGL: GLInterface {
-    private val internalImpl: GLInterface
-
-    init {
-        val caps = GL.getCapabilities()
-        internalImpl = when {
-            caps.OpenGL32 -> {
-                glslVersion = 150
-                ImplGL3()
+class ImplBestGL: GLInterface by getNewImpl() {
+    companion object {
+        private val caps = GL.getCapabilities()
+        private fun getNewImpl(): GLInterface {
+            return when {
+                caps.OpenGL32 -> {
+                    glslVersion = 150
+                    ImplGL3()
+                }
+                caps.OpenGL30 && Platform.get() != Platform.MACOSX -> {
+                    glslVersion = 130
+                    ImplGL3()
+                }
+                caps.OpenGL20 -> {
+                    glslVersion = 110
+                    if (Platform.get() == Platform.MACOSX) ImplGL2_mac() else ImplGL2()
+                }
+                else -> throw RuntimeException("OpenGL 2 is not present on this system!")
             }
-            caps.OpenGL30 && Platform.get() != Platform.MACOSX -> {
-                glslVersion = 130
-                ImplGL3()
-            }
-            caps.OpenGL20 -> {
-                glslVersion = 110
-                if (Platform.get() == Platform.MACOSX) ImplGL2_mac() else ImplGL2()
-            }
-            else -> throw RuntimeException("OpenGL 2 is not present on this system!")
         }
     }
-
-    override fun shutdown() = internalImpl.shutdown()
-    override fun newFrame() = internalImpl.newFrame()
-    override fun renderDrawData(drawData: DrawData) = internalImpl.renderDrawData(drawData)
-    override fun createFontsTexture() = internalImpl.createFontsTexture()
-    override fun destroyFontsTexture() = internalImpl.destroyFontsTexture()
-    override fun createDeviceObjects() = internalImpl.createDeviceObjects()
-    override fun destroyDeviceObjects() = internalImpl.destroyDeviceObjects()
 }
