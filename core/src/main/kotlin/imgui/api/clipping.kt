cc8124e15537e4fcb184e0b6f7313d152a9c07e1
Giuseppe Barbieri <elect86@gmail.com>
2020-09-14 11:34:25 +0200

    :construction:    Columns: Lower overhead on column switches and switching to background channel (some stress tests in debug builds went 3->2 ms). (#125)
                     This change benefits Columns but was primarily made with Tables in mind.

--- core/src/main/kotlin/imgui/api/clipping.kt
+++ core/src/main/kotlin/imgui/api/clipping.kt
@@ -6,8 +6,12 @@ import imgui.ImGui.currentWindow
 /** Clipping */
 interface clipping {
 
-    /** When using this function it is sane to ensure that float are perfectly rounded to integer values, to that e.g.
-    (int)(max.x-min.x) in user's render produce correct result. */
+    /** Push a clipping rectangle for both ImGui logic (hit-testing etc.) and low-level ImDrawList rendering.
+     *  - When using this function it is sane to ensure that float are perfectly rounded to integer values,
+     *    so that e.g. (int)(max.x-min.x) in user's render produce correct result.
+     *  - If the code here changes, may need to update code of functions like NextColumn() and PushColumnClipRect():
+     *    some frequently called functions which to modify both channels and clipping simultaneously tend to use a more
+     *    specialized code path to added extraneous updates of the underlying ImDrawCmd. */
     fun pushClipRect(clipRectMin: Vec2, clipRectMax: Vec2, intersectWithCurrentClipRect: Boolean) {
         currentWindow.apply {
             drawList.pushClipRect(clipRectMin, clipRectMax, intersectWithCurrentClipRect)
