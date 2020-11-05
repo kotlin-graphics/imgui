9c8a131c2cf4e9c2845e658146f99f89a0bd52af
Giuseppe Barbieri <elect86@gmail.com>
2020-11-05 12:01:59 +0100

    :construction: Scrolling: Fixed edge snapping being applied prior to knowing ContentSize. (#3452)

--- core/src/main/kotlin/imgui/api/windowScrolling.kt
+++ core/src/main/kotlin/imgui/api/windowScrolling.kt
@@ -41,33 +41,18 @@ interface windowScrolling {
     val scrollMaxY: Float
         get() = currentWindow.scrollMax.y
 
-    /** Tweak: snap on edges when aiming at an item very close to the edge,
-     *  So the difference between WindowPadding and ItemSpacing will be in the visible area after scrolling.
-     *  When we refactor the scrolling API this may be configurable with a flag?
-     *  Note that the effect for this won't be visible on X axis with default Style settings as WindowPadding.x == ItemSpacing.x by default. */
-    fun calcScrollSnap(target: Float, snapMin: Float, snapMax: Float, snapThreshold: Float, centerRatio: Float): Float =
-        when {
-            target <= snapMin + snapThreshold -> lerp(snapMin, target, centerRatio)
-            target >= snapMax - snapThreshold -> lerp(target, snapMax, centerRatio)
-            else -> target
-        }
-
     /** center_x_ratio: 0.0f left of last item, 0.5f horizontal center of last item, 1.0f right of last item.
      *
      *  adjust scrolling amount to make current cursor position visible. center_x_ratio=0.0: left, 0.5: center, 1.0: right. When using to make a "default/current item" visible, consider using SetItemDefaultFocus() instead. */
     fun setScrollHereX(centerXRatio: Float) {
         val window = g.currentWindow!!
         val spacingX = style.itemSpacing.x
-        var targetPosX =
+        val targetPosX =
             lerp(window.dc.lastItemRect.min.x - spacingX, window.dc.lastItemRect.max.x + spacingX, centerXRatio)
+        window.setScrollFromPosX(targetPosX - window.pos.x, centerXRatio) // Convert from absolute to local pos
 
         // Tweak: snap on edges when aiming at an item very close to the edge
-        val snapXThreshold = 0f max (window.windowPadding.x - spacingX)
-        val snapXMin = window.dc.cursorStartPos.x - window.windowPadding.x
-        val snapXMax = window.dc.cursorStartPos.x + window.contentSize.x + window.windowPadding.x
-        targetPosX = calcScrollSnap(targetPosX, snapXMin, snapXMax, snapXThreshold, centerXRatio)
-
-        window.setScrollFromPosX(targetPosX - window.pos.x, centerXRatio) // Convert from absolute to local pos
+        window.scrollTargetEdgeSnapDist.x = 0f max (window.windowPadding.x - spacingX)
     }
 
     /** adjust scrolling amount to make current cursor position visible.
@@ -76,17 +61,13 @@ interface windowScrolling {
     fun setScrollHereY(centerYRatio: Float = 0.5f) {
         val window = g.currentWindow!!
         val spacingY = style.itemSpacing.y
-        var targetPosY = lerp(window.dc.cursorPosPrevLine.y - spacingY,
+        val targetPosY = lerp(window.dc.cursorPosPrevLine.y - spacingY,
             window.dc.cursorPosPrevLine.y + window.dc.prevLineSize.y + spacingY,
             centerYRatio)
+        window.setScrollFromPosY(targetPosY - window.pos.y, centerYRatio) // Convert from absolute to local pos
 
         // Tweak: snap on edges when aiming at an item very close to the edge
-        val snapYThreshold = 0f max (window.windowPadding.y - spacingY)
-        val snapYMin = window.dc.cursorStartPos.y - window.windowPadding.y
-        val snapYMax = window.dc.cursorStartPos.y + window.contentSize.y + window.windowPadding.y
-        targetPosY = calcScrollSnap(targetPosY, snapYMin, snapYMax, snapYThreshold, centerYRatio)
-
-        window.setScrollFromPosY(targetPosY - window.pos.y, centerYRatio) // Convert from absolute to local pos
+        window.scrollTargetEdgeSnapDist.y = 0f max (window.windowPadding.y - spacingY)
     }
 
     fun setScrollFromPosX(localX: Float, centerXratio: Float) =
