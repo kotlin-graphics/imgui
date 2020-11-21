5b38122e5482c2b20a2cc2f00f59f93796d8b390
Giuseppe Barbieri <elect86@gmail.com>
2020-11-21 04:32:59 +0100

    vulkan crashes at instance, nice

--- vk/src/test/kotlin/imgui/vulkan.kt
+++ vk/src/test/kotlin/imgui/vulkan.kt
@@ -62,72 +62,72 @@ fun main() {
     setupVulkan(glfw.requiredInstanceExtensions)
 
     // Create Window Surface
-    val surface = gInstance createSurface window
-
-    // Create Framebuffers
-    val size = window.framebufferSize
-    val wd = gMainWindowData
-    setupVulkanWindow(wd, surface, size)
-
-    // Setup Dear ImGui context
-    val context = Context()
-    val io = ImGui.io
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-
-    // Setup Dear ImGui style
-    ImGui.styleColorsDark()
-    //ImGui::StyleColorsClassic();
-
-    // Setup Platform/Renderer bindings
-    val implGlfw = ImplGlfw.initForVulkan(window, true)
-    val initInfo = ImplVulkan.InitInfo().also {
-        it.instance = gInstance
-        it.physicalDevice = gPhysicalDevice
-        it.device = gDevice
-        it.queueFamily = gQueueFamily
-        it.queue = gQueue
-        it.pipelineCache = gPipelineCache
-        it.descriptorPool = gDescriptorPool
-        it.minImageCount = gMinImageCount
-        it.imageCount = wd.imageCount
-    }
-    val implVk = ImplVulkan(initInfo, wd.renderPass)
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-
-    // Upload Fonts
-    run {
-        // Use any command queue
-        val commandPool = wd.frames!![wd.frameIndex].commandPool
-        val commandBuffer = wd.frames!![wd.frameIndex].commandBuffer!!
-
-        gDevice.resetCommandPool(commandPool, 0)
-        val beginInfo = CommandBufferBeginInfo(flags = VkCommandBufferUsage.ONE_TIME_SUBMIT_BIT.i)
-        commandBuffer.begin(beginInfo)
-
-        implVk.createFontsTexture(commandBuffer)
-
-        val endInfo = SubmitInfo(commandBuffer = commandBuffer)
-        commandBuffer.end()
-        gQueue.submit(endInfo)
-
-        gDevice.waitIdle()
-        implVk.destroyFontUploadObjects()
-    }
+//    val surface = gInstance createSurface window
+//
+//    // Create Framebuffers
+//    val size = window.framebufferSize
+//    val wd = gMainWindowData
+//    setupVulkanWindow(wd, surface, size)
+//
+//    // Setup Dear ImGui context
+//    val context = Context()
+//    val io = ImGui.io
+//    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+//    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+//
+//    // Setup Dear ImGui style
+//    ImGui.styleColorsDark()
+//    //ImGui::StyleColorsClassic();
+//
+//    // Setup Platform/Renderer bindings
+//    val implGlfw = ImplGlfw.initForVulkan(window, true)
+//    val initInfo = ImplVulkan.InitInfo().also {
+//        it.instance = gInstance
+//        it.physicalDevice = gPhysicalDevice
+//        it.device = gDevice
+//        it.queueFamily = gQueueFamily
+//        it.queue = gQueue
+//        it.pipelineCache = gPipelineCache
+//        it.descriptorPool = gDescriptorPool
+//        it.minImageCount = gMinImageCount
+//        it.imageCount = wd.imageCount
+//    }
+//    val implVk = ImplVulkan(initInfo, wd.renderPass)
+//
+//    // Load Fonts
+//    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+//    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+//    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+//    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+//    // - Read 'docs/FONTS.md' for more instructions and details.
+//    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+//    //io.Fonts->AddFontDefault();
+//    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+//    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+//    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+//    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+//    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+//    //IM_ASSERT(font != NULL);
+//
+//    // Upload Fonts
+//    run {
+//        // Use any command queue
+//        val commandPool = wd.frames!![wd.frameIndex].commandPool
+//        val commandBuffer = wd.frames!![wd.frameIndex].commandBuffer!!
+//
+//        gDevice.resetCommandPool(commandPool, 0)
+//        val beginInfo = CommandBufferBeginInfo(flags = VkCommandBufferUsage.ONE_TIME_SUBMIT_BIT.i)
+//        commandBuffer.begin(beginInfo)
+//
+//        implVk.createFontsTexture(commandBuffer)
+//
+//        val endInfo = SubmitInfo(commandBuffer = commandBuffer)
+//        commandBuffer.end()
+//        gQueue.submit(endInfo)
+//
+//        gDevice.waitIdle()
+//        implVk.destroyFontUploadObjects()
+//    }
 
     // Our state
 //    bool show_demo_window = true
@@ -213,10 +213,10 @@ fun main() {
 //    }
 
     // Cleanup
-    gDevice.waitIdle()
-    implVk.shutdown()
-    implGlfw.shutdown()
-    context.destroy()
+//    gDevice.waitIdle()
+//    implVk.shutdown()
+//    implGlfw.shutdown()
+//    context.destroy()
 
 //    cleanupVulkanWindow()
 //    cleanupVulkan()
