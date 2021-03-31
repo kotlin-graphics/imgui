ddb8ec025625d5e66a1af567f334ad5cc543bc23
Giuseppe Barbieri <elect86@gmail.com>
2021-03-31 12:42:23 +0200

    build logic extraction

--- gl/src/main/java/module-info.java
+++ gl/src/main/java/module-info.java
@@ -1,16 +1,16 @@
-module com.github.kotlin_graphics.imgui.gl {
+module kotlin.graphics.imgui.gl {
 
     requires java.desktop;
 
     requires kotlin.stdlib;
 
-    requires com.github.kotlin_graphics.imgui.core;
-    requires com.github.kotlin_graphics.imgui.glfw;
-    requires com.github.kotlin_graphics.uno.core;
-    requires com.github.kotlin_graphics.glm;
-    requires com.github.kotlin_graphics.gln;
-    requires com.github.kotlin_graphics.kool;
-    requires com.github.kotlin_graphics.kotlin_unsigned;
+    requires kotlin.graphics.imgui.core;
+    requires kotlin.graphics.imgui.glfw;
+    requires kotlin.graphics.uno.core;
+    requires kotlin.graphics.glm;
+    requires kotlin.graphics.gln;
+    requires kotlin.graphics.kool;
+    requires kotlin.graphics.unsigned;
 
     requires org.lwjgl.opengl;
     requires org.lwjgl.glfw;
