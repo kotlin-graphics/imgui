d0c67caae45671f259934b300b66aad876573524
Giuseppe Barbieri <elect86@gmail.com>
2021-10-09 15:21:52 +0200

    :construction: Version 1.81 WIP

--- core/src/main/kotlin/imgui/imgui.kt
+++ core/src/main/kotlin/imgui/imgui.kt
@@ -15,7 +15,7 @@ import kool.Stack
 const val IMGUI_BUILD = 0
 
 /** get the compiled version string e.g. "1.80 WIP" (essentially the value for IMGUI_VERSION from the compiled version of imgui.cpp) */
-const val IMGUI_VERSION = "1.80"
+const val IMGUI_VERSION = "1.81 WIP"
 const val IMGUI_VERSION_BUILD = "$IMGUI_VERSION.$IMGUI_BUILD"
 /** Integer encoded as XYYZZ for use in #if preprocessor conditionals.
 Work in progress versions typically starts at XYY99 then bounce up to XYY00, XYY01 etc. when release tagging happens) */
