3a2ffaf244664870305e9c557de035f30f40a766
Giuseppe Barbieri <elect86@gmail.com>
2023-01-22 14:29:37 +0100

    :construction: (broken) Version 1.81

--- core/src/main/kotlin/imgui/imgui.kt
+++ core/src/main/kotlin/imgui/imgui.kt
@@ -15,11 +15,11 @@ import kool.Stack
 const val IMGUI_BUILD = 0
 
 /** get the compiled version string e.g. "1.80 WIP" (essentially the value for IMGUI_VERSION from the compiled version of imgui.cpp) */
-const val IMGUI_VERSION = "1.81 WIP"
+const val IMGUI_VERSION = "1.81"
 const val IMGUI_VERSION_BUILD = "$IMGUI_VERSION.$IMGUI_BUILD"
 /** Integer encoded as XYYZZ for use in #if preprocessor conditionals.
 Work in progress versions typically starts at XYY99 then bounce up to XYY00, XYY01 etc. when release tagging happens) */
-const val IMGUI_VERSION_NUM = 18004
+const val IMGUI_VERSION_NUM = 18100
 
 
 // Helpers macros to generate 32-bits encoded colors
