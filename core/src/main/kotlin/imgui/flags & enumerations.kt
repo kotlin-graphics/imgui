db71ff0aed0aa3a4e5cf11e400fcb1632b61cb69
Giuseppe Barbieri <elect86@gmail.com>
2023-01-24 18:28:04 +0100

    :construction: (broken) Tables: offset and shuffle flags (breaks ABI compatibility as often)
                            If you rely on ABI compatibility consider reworking how your backends are created, using cimgui metadata output. We will happily often break ABI compat.

--- core/src/main/kotlin/imgui/flags & enumerations.kt
+++ core/src/main/kotlin/imgui/flags & enumerations.kt
@@ -763,69 +763,69 @@ enum class TableColumnFlag(@JvmField val i: TableColumnFlags) {
     None(0),
 
     /** Default as a hidden/disabled column. */
-    DefaultHide(1 shl 0),
+    DefaultHide(1 shl 1),
 
     /** Default as a sorting column. */
-    DefaultSort(1 shl 1),
+    DefaultSort(1 shl 2),
 
     /** Column will stretch. Preferable with horizontal scrolling disabled (default if table sizing policy is _SizingStretchSame or _SizingStretchProp). */
-    WidthStretch(1 shl 2),
+    WidthStretch(1 shl 3),
 
     /** Column will not stretch. Preferable with horizontal scrolling enabled (default if table sizing policy is _SizingFixedFit and table is resizable). */
-    WidthFixed(1 shl 3),
+    WidthFixed(1 shl 4),
 
     /** Disable manual resizing. */
-    NoResize(1 shl 4),
+    NoResize(1 shl 5),
 
     /** Disable manual reordering this column, this will also prevent other columns from crossing over this column. */
-    NoReorder(1 shl 5),
+    NoReorder(1 shl 6),
 
     /** Disable ability to hide/disable this column. */
-    NoHide(1 shl 6),
+    NoHide(1 shl 7),
 
     /** Disable clipping for this column (all NoClip columns will render in a same draw command). */
-    NoClip(1 shl 7),
+    NoClip(1 shl 8),
 
     /** Disable ability to sort on this field (even if ImGuiTableFlags_Sortable is set on the table). */
-    NoSort(1 shl 8),
+    NoSort(1 shl 9),
 
     /** Disable ability to sort in the ascending direction. */
-    NoSortAscending(1 shl 9),
+    NoSortAscending(1 shl 10),
 
     /** Disable ability to sort in the descending direction. */
-    NoSortDescending(1 shl 10),
-
-    /** Disable header text width contribution to automatic column width. */
-    NoHeaderWidth(1 shl 11),
-
-    /** Make the initial sort direction Ascending when first sorting on this column (default). */
-    PreferSortAscending(1 shl 12),
-
-    /** Make the initial sort direction Descending when first sorting on this column. */
-    PreferSortDescending(1 shl 13),
-
-    /** Use current Indent value when entering cell (default for column 0). */
-    IndentEnable(1 shl 14),
-
-    /** Ignore current Indent value when entering cell (default for columns > 0). Indentation changes _within_ the cell will still be honored. */
-    IndentDisable(1 shl 15),
+    NoSortDescending(1 shl 11),
 
     /** TableHeadersRow() will not submit label for this column. Convenient for some small columns. Name will still appear in context menu. */
-    NoHeaderLabel(1 shl 16),
+    NoHeaderLabel(1 shl 12),
+
+    /** Disable header text width contribution to automatic column width. */
+    NoHeaderWidth(1 shl 13),
+
+    /** Make the initial sort direction Ascending when first sorting on this column (default). */
+    PreferSortAscending(1 shl 14),
+
+    /** Make the initial sort direction Descending when first sorting on this column. */
+    PreferSortDescending(1 shl 15),
+
+    /** Use current Indent value when entering cell (default for column 0). */
+    IndentEnable(1 shl 16),
+
+    /** Ignore current Indent value when entering cell (default for columns > 0). Indentation changes _within_ the cell will still be honored. */
+    IndentDisable(1 shl 17),
 
     // Output status flags, read-only via TableGetColumnFlags()
 
     /** Status: is enabled == not hidden by user/api (referred to as "Hide" in _DefaultHide and _NoHide) flags. */
-    IsEnabled(1 shl 20),
+    IsEnabled(1 shl 24),
 
     /** Status: is visible == is enabled AND not clipped by scrolling. */
-    IsVisible(1 shl 21),
+    IsVisible(1 shl 25),
 
     /** Status: is currently part of the sort specs */
-    IsSorted(1 shl 22),
+    IsSorted(1 shl 26),
 
     /** Status: is hovered by mouse */
-    IsHovered(1 shl 23),
+    IsHovered(1 shl 27),
 
     // [Internal] Combinations and masks
 
