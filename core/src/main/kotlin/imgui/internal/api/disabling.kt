1089fb6281cfb2acc986ed40344988ddfaa3e127
Giuseppe Barbieri <elect86@gmail.com>
2023-01-26 10:04:17 +0100

    :construction: (broken) Fix Fix BeginDisabled(false), again, (#211, #4452, #4453) Version 1.84.1
                                (forced pushed since our earlier versioning didn't sort correctly in github web)

--- core/src/main/kotlin/imgui/internal/api/disabling.kt
+++ core/src/main/kotlin/imgui/internal/api/disabling.kt
@@ -1,29 +1,25 @@
 package imgui.internal.api
 
-import imgui.ImGui
 import imgui.api.g
-import imgui.internal.sections.ItemFlag
-import imgui.internal.sections.div
-import imgui.internal.sections.minus
-import imgui.internal.sections.has
-import imgui.internal.sections.hasnt
+import imgui.internal.sections.*
 
 // Disabling [BETA API]
 // - Disable all user interactions and dim items visuals (applying style.DisabledAlpha over current colors)
+// - BeginDisabled(false) essentially does nothing useful but is provided to facilitate use of boolean expressions. If you can avoid calling BeginDisabled(False)/EndDisabled() best to avoid it.
 internal interface disabling {
 
     /** BeginDisabled()/EndDisabled()
      *  - Those can be nested but this cannot be used to enable an already disabled section (a single BeginDisabled(true) in the stack is enough to keep things disabled)
      *  - Visually this is currently altering alpha, but it is expected that in a future styling system this would work differently.
      *  - Feedback welcome at https://github.com/ocornut/imgui/issues/211
-     *  - BeginDisabled(false) essentially does nothing but is provided to facilitate use of boolean expressions
+     *  - BeginDisabled(false) essentially does nothing useful but is provided to facilitate use of boolean expressions. If you can avoid calling BeginDisabled(False)/EndDisabled() best to avoid it.
      *  - Optimized shortcuts instead of PushStyleVar() + PushItemFlag() */
     fun beginDisabled(disabled: Boolean = true) {
         val wasDisabled = g.currentItemFlags has ItemFlag.Disabled
         g.disabledAlphaBackup = g.style.alpha
         if (!wasDisabled && disabled)
             g.style.alpha *= g.style.disabledAlpha // PushStyleVar(ImGuiStyleVar_Alpha, g.Style.Alpha * g.Style.DisabledAlpha);
-        //PushItemFlag(ImGuiItemFlags_Disabled, was_disabled || disabled);
+        if (wasDisabled || disabled)
         g.currentItemFlags /= ItemFlag.Disabled
         g.itemFlagsStack += g.currentItemFlags
     }
