d76797d50ea763bfca1610a540bdcc2f99bc9c0a
Giuseppe Barbieri <elect86@gmail.com>
2023-06-01 16:30:53 +0200

    v1.89.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 gradle.rootProject {
     group = "kotlin.graphics"
-    version = "1.89.2"
+    version = "1.89.3"
 }
 
 dependencyResolutionManagement {
